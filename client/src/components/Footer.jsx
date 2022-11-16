914395a7dcd1bc70d721782fc66eef93dfc7eab9
Isaac Tsibu-Darko <isaactsibudarko@gmail.com>
2022-11-16 02:34:34 +0000

    trying to create a post build command inside root

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
             <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
             <div className="flex flex-[0.5] justify-center items-center">
-                <img src={logo} alt="logo" className="w-16 h-16" />
+                <img src={logo} alt="logo" className="w-12 h-12" />
             </div>
             <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
                 <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
@@ -17,15 +17,15 @@ const Footer = () => {
             </div>
 
             <div className="flex justify-center items-center flex-col mt-5">
-            <p className="text-white text-sm text-center">Come join us and hear for the unexpected miracle</p>
-            <p className="text-white text-sm text-center font-medium mt-2">crypto@ozgalaxy.com</p>
+            <p className="text-white text-sm text-center"></p>
+           
             </div>
 
             <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
             <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-            <p className="text-white text-left text-xs">@SignetGlobal 2022</p>
-            <p className="text-white text-right text-xs">All rights reserved</p>
+            <p className="text-white text-left text-xs">crypto@ozgalaxy.com</p>
+            <p className="text-white text-right text-xs">@SignetGlobal 2022 All rights reserved</p>
             </div>
         </div>
     )
--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -38,7 +38,7 @@ const TransactionCard = ({ url, message, timestamp, keyword, addressFrom, amount
                 <img
                 src={gifUrl || url}
                 alt="nature"
-                className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
+                className="w-full h-34 2xl:h-86 rounded-md shadow-lg object-cover"
                 />
                 <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
                 <p className="text-[#37c7da] font-bold">{timestamp}</p>
